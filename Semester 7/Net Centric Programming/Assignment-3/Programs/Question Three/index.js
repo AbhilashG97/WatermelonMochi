8633e6ff70662ba2dcf7b456bd7de5a54d9d7212
Abhilash G <abhilash.ag1997@gmail.com>
2019-07-10 17:09:45 +0530

    Assignment 3: Question 3, done! :sparkles:

--- Semester 7/Net Centric Programming/Assignment-3/Programs/Question Three/index.js
+++ Semester 7/Net Centric Programming/Assignment-3/Programs/Question Three/index.js
@@ -1,7 +1,31 @@
 'use strict'
 
-let customer = new Object();
+let customer = {
+    username : "",
+    phoneNumber : "",
+    email : "",
+    taxiType : "",
+    pickupDateAndTime : "",
+    pickUpPlace : "",
+    dropOffPlace : "",
+    specialInstructions : "",
+};
 
+/**
+ * This function gets all the values 
+ * from various fields and assigns them 
+ * to an object
+ */
 function onSubmitButtonPressed() {
-    alert("Button Pressed!")
+
+    customer.username = $('#userNameField').val();
+    customer.phoneNumber = $('#phoneNumberField').val();
+    customer.email = $('#emailField').val();
+    customer.taxiType = $("input[name='taxiType']:checked").val();
+    customer.pickupDateAndTime = $('#dateTimeField').val();
+    customer.pickUpPlace = $('#pickUp option:selected').text();
+    customer.dropOffPlace = $('#dropOff option:selected').text();
+    customer.specialInstructions = $('#instructionsTextarea').val();
+
+    alert("Form submitted successfully!");
 }
